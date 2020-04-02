349d5682b434a6f1b8674d286a468032459282e9
Tiberiu <tiberiuiuliann@yahoo.com>
2020-04-02 19:00:57 +0300

    Create-generic-footer

--- CategoryPage/categoryPage.js
+++ CategoryPage/categoryPage.js
@@ -22,3 +22,27 @@
          throw new Error(e);
      }
  }
+
+ const loadFooter = async() => {
+     const footerContainer= document.getElementById('#footer-container');
+     const footerPath = '../Generics/footer.html';
+
+     try {
+         await fetch(footerPath)
+             .then(response => response.text())
+             .then(html => {
+                 footerContainer.innerHTML=html;
+
+             })
+             .catch(error => {
+                 console.log(error);
+             })
+
+     } catch (e) {
+         throw new Error(e);
+     }
+ }
+    function load (){
+        loadCards(5);
+        loadFooter();
+    }
