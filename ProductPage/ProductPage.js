663bcd6956ca2efb068b7d53e66cf0adfc0ea1a6
tiberiu1999 <tiberiuiuliann@yahoo.com>
2020-04-29 16:05:42 +0300

    Solving-footer

--- ProductPage/ProductPage.js
+++ ProductPage/ProductPage.js
@@ -1,9 +1,9 @@
 
-import { loadFooter } from "../Generics/Footer/footer.js";
+import { loadFooter } from "../Components/Generics/Footer/footer.js";
 
 
 const initialize = async () => {
-    const htmlFooterPath = '../Generics/Footer/footer.html';
+    const htmlFooterPath = '../Components/Generics/Footer/footer.html';
     const idFooterContainer = '#footer-container';
 
     await loadFooter(idFooterContainer, htmlFooterPath);
